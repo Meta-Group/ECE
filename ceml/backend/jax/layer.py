6e37f8b21b6b20dfc18acd6d28c98594a0a8a0b0
rzfzr <capitao.rafa@gmail.com>
2025-12-17 22:18:12 +0100

    Update jax config import

--- ceml/backend/jax/layer.py
+++ ceml/backend/jax/layer.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import jax
-from jax.config import config; config.update("jax_enable_x64", True)    # Use 64 bit floating point numbers
+jax.config.update("jax_enable_x64", True)    # Use 64 bit floating point numbers
 import jax.numpy as npx
 
 
